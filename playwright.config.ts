afa38cf4c21d5e223463673251a470897e8d4cce
Gagan Suie <gagan.suie@codecrow.io>
2022-11-14 02:33:25 -0600

    Fix: testing playwright changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,24 +1,12 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-    testDir: './tests',
-    timeout: 30 * 1000,
-    expect: {
-        timeout: 5000
-    },
-    fullyParallel: true,
-    forbidOnly: !!process.env.CI,
-    retries: process.env.CI ? 2 : 0,
-    workers: process.env.CI ? 4 : undefined,
     reporter: [['json', { outputFile: 'e2eresults.json' }]],
-    use: {
-        actionTimeout: 0,
-        trace: 'on-first-retry',
-    },
     webServer: {
         command: 'npm run dev',
         port: 4200,
-    }
+    },
+    workers: process.env.CI ? 4 : undefined,
 }
 
 export default config
