ebb2bc6296006060844e3f2282bab312d5a2d25e
Gagan Suie <gagan.suie@codecrow.io>
2022-11-14 03:15:25 -0600

    Fix: added mergify yml file and testing playwright change

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,12 @@ const config: PlaywrightTestConfig = {
     reporter: [['json', { outputFile: 'e2eresults.json' }]],
     webServer: {
         command: 'npm run dev',
-        port: 4200,
+        url: 'http://localhost:4200/',
+        timeout: 120000,
+        reuseExistingServer: !process.env.CI,
+    },
+    use: {
+        baseURL: 'http://localhost:4200/',
     },
     workers: process.env.CI ? 4 : undefined,
 }
